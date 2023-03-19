50af17629d948c5ae25ae498a49a882846353483
xqxls <1078774751@qq.com>
2023-03-19 19:31:33 +0800

    修改接口返回的公共类

--- src/main/java/com/example/myseckill/config/WebConfig.java
+++ src/main/java/com/example/myseckill/config/WebConfig.java
@@ -33,7 +33,7 @@ public class WebConfig implements WebMvcConfigurer {
     //静态资源展示
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/templates/");
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
         //swagger 和 knife4j
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
